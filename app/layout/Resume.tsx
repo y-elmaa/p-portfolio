af6c50f717cea048ed16e00da7a259d9ccf569d2
y-elmaa <elmaaroufiyahya19@gmail.com>
2025-06-15 21:22:40 +0100

    force dark mode

--- app/layout/Resume.tsx
+++ app/layout/Resume.tsx
@@ -1,23 +1,19 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 const Resume = () => {
   return (
     <section id="Resume" className="flex justify-center items-center p-8">
-      <a href="Yahya Elmaaroufi CV.jpg" download='Yahya Elmaaroufi CV.jpg'>
-         <Image
-        src="/Yahya Elmaaroufi CV.jpg"
-        alt="resume"
-        width={1000} 
-        height={1414} 
-         
-        priority 
-      />
+      <a href="Yahya Elmaaroufi CV.pdf" download="Yahya Elmaaroufi CV.pdf">
+        <Image
+          src="/Yahya Elmaaroufi CV.jpg"
+          alt="resume"
+          width={1000}
+          height={1414}
+          priority
+        />
       </a>
+    </section>
+  );
+};
 
-</section>
-
-  )
-}
-
-export default Resume
-
+export default Resume;
