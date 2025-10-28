2f8035d573b1dc7d33d6584a0898f1daa86ffd45
y-elmaa <elmaaroufiyahya19@gmail.com>
2025-10-28 18:53:35 +0100

    add socialmedia

--- app/components/content.ts
+++ app/components/content.ts
@@ -7,6 +7,12 @@ export const projects = [
     image: "issues-tracker.jpg",
   },
   {
+  Title: "SocialMedia",
+  Description:"SocialMedia est une application combinant réseau social et messagerie en temps réel, construite avec Next.js, Prisma et PostgreSQL. Elle utilise NextAuth pour l’authentification, Cloudinary pour les images et Pusher pour le chat.",
+  Tech: ["Next.js", "Prisma", "NextAuth", "PostgreSQL", "Cloudinary", "Pusher"],
+}
+,
+  {
     Title: "Facebook Post Clone",
     Description:
       "Facebook Post Clone est une application web permettant de publier, aimer et commenter des posts, construite avec Next.js et Prisma, utilisant Cloudinary pour le stockage des images et NextAuth pour la gestion des utilisateurs.",
@@ -46,6 +52,8 @@ export const projects = [
 
 export const links = [
   "https://issues-tracker-tan.vercel.app/issues",
+  "https://socialmedia-seven-psi.vercel.app/",
+
   "https://facbook-post.vercel.app/",
   "https://chatapp-six-kappa.vercel.app/",
   "https://github.com/y-elmaa/ToDoList",
