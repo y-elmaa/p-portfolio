4eb770e4b498fd4bd7b552dd657da04bc2297a3e
y-elmaa <elmaaroufiyahya19@gmail.com>
2025-10-31 11:10:33 +0100

    1

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import About from "./layout/About";
-import Resume from "./layout/Resume";
 import Projects from "./layout/Projects";
 import Contact from "./layout/Contact";
 import HomePage from "./layout/home";
